5e0ec698838cd7239b6ba9c04ab5965e05bef7b1
JU HYEONSU(notebook) <1434gus@tukorea.ac.kr>
2025-09-17 11:56:57 +0900

    #3 initial commit(이름만 변경)
    #2에서 복사하여 이름만 변경

---
+++ character_move.py
@@ -0,0 +1,48 @@
+from pico2d import * # pico2d.open_canvas() 앞에 안붙여도 됨 all 임포트
+
+import math
+x = 400
+y = 90
+speed = 10
+
+def boy_draw(x, y):
+    clear_canvas()
+    grass.draw_now(400, 30)
+    character.draw_now(x, y)
+    delay(0.01)
+
+
+open_canvas()
+hide_lattice()
+
+grass = load_image('grass.png')
+character = load_image('character.png')
+
+while 1:
+    # 사각이동
+    while x < 750:
+        x += speed
+        boy_draw(x, y)
+    while y < 510:
+        y += speed
+        boy_draw(x, y)
+    while x > 50:
+        x -= speed
+        boy_draw(x, y)
+        # 원 이동 (중심: 400, 300, 반지름: 210)
+        if x == 400 and y == 510:
+            for d in range(0, 361, 5):
+                rad = math.radians(d)
+                x = 400 + 210 * math.sin(rad)
+                y = 300 + 210 * math.cos(rad)
+                boy_draw(x, y)
+
+    while y != 90:
+        y -= speed
+        boy_draw(x, y)
+
+
+
+close_canvas()
+
+
